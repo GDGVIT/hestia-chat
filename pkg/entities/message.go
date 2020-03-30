ced40d4dd2e06e57733931d1fd2f8f7fd3ea5380
ATechnoHazard <amolele@gmail.com>
2020-03-30 19:32:59 +0530

    fix a regression with incorrect json tag being used
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- pkg/entities/message.go
+++ pkg/entities/message.go
@@ -5,6 +5,6 @@ import "github.com/jinzhu/gorm"
 type Message struct {
 	gorm.Model
 	ReceiverRefer uint   `json:"receiver" gorm:"not null"`
-	Sender        uint   `json:"from" gorm:"not null"`
+	Sender        uint   `json:"sender" gorm:"not null"`
 	Text          string `json:"text" gorm:"not null"`
 }
