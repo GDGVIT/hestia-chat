4785dea324aa7dd68e69050e8a840f5e239cf776
ATechnoHazard <amolele@gmail.com>
2020-04-02 01:36:24 +0530

    Fix issue with cross connections in websockets
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- api/websocket/websocket.go
+++ api/websocket/websocket.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-var Clients = make(map[uint]*websocket.Conn)
+var Clients = make(map[sendKey]*websocket.Conn)
 var Broadcast = make(chan entities.Message, 5)
 var upgrader = websocket.FastHTTPUpgrader{
 	CheckOrigin: func(ctx *fasthttp.RequestCtx) bool {
@@ -18,20 +18,27 @@ var upgrader = websocket.FastHTTPUpgrader{
 	WriteBufferSize: 1024,
 }
 
+type sendKey struct {
+	Sender   uint
+	Receiver uint
+}
+
 func HandleConns(ctx *fasthttp.RequestCtx) {
-	chatID, _ := strconv.Atoi(string(ctx.QueryArgs().Peek("chat")))
+	sender, _ := strconv.Atoi(string(ctx.QueryArgs().Peek("sender")))
+	receiver, _ := strconv.Atoi(string(ctx.QueryArgs().Peek("receiver")))
+
 	err := upgrader.Upgrade(ctx, func(conn *websocket.Conn) {
-		Clients[uint(chatID)] = conn
+		Clients[sendKey{Sender: uint(sender), Receiver: uint(receiver)}] = conn
 		defer func() {
 			if r := recover(); r != nil {
 				log.Println("recovered from panic")
 			}
 		}()
-		log.Printf("Connecting to websocket with chatID %v ", chatID)
+		log.Printf("Connecting to websocket with sender %v ", sender)
 		select {
 		case msg := <-Broadcast:
 			log.Println(msg, Clients)
-			err := Clients[msg.ReceiverRefer].WriteJSON(msg)
+			err := Clients[sendKey{Sender: msg.Sender, Receiver: msg.ReceiverRefer}].WriteJSON(msg)
 			if err != nil {
 				log.Println("Error writing to client", "err", err, "client", conn)
 				_ = conn.Close()
